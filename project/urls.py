c90a78f8ef465d0b5c8530b3ffc4686bf094ac67
Gautam Mehar <gautammehar792@gmail.com>
2025-05-17 15:01:39 +0530

    change app name
    change app name

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('talenthive.urls')),  # Include all URLs from the talenthive app
+    path('', include('talent.urls')),  # Include all URLs from the talent app
 ]
 
 if settings.DEBUG:
