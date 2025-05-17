b47800ad4b892cd44278d8c1f0f904c957aa1c0d
Gautam Mehar <gautammehar792@gmail.com>
2025-05-17 17:49:49 +0530

    hwg2

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = [
+    'talenthive.onrender.com',
+    'talenthive-c0jn.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 # Application definition
 
