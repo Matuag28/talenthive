0b1b6a383c71ed72d23b198f729c05cc3cac6973
Gautam Mehar <gautammehar792@gmail.com>
2025-05-18 16:11:48 +0530

    make post gre db
    make post gre db

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('TalentHive.urls')),
+    path('', include('talent.urls')),
 ]
 
 # Add media URL configuration
